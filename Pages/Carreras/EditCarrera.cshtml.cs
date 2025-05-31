dfa3eeeaedbb21e9c2d76d5db9de7de58c0e93fb
pablobenitezpasserini <pablobenitez252@gmail.com>
2025-05-30 23:31:41 -0300

    reorganizacion de vistas

---
+++ Pages/Carreras/EditCarrera.cshtml.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SistemaAcademico.Data;
+using SistemaAcademico.Models;
+
+namespace SistemaAcademico.Pages.Carreras
+{
+	public class EditCarreraModel : PageModel
+	{
+		[BindProperty]
+		public Carrera oCarrera { get; set; }
+		public void OnGet(int id)
+		{
+			foreach (var carrera in DatosCompartidos.ListCarrera)
+			{
+				if (carrera.Id == id)
+				{
+					oCarrera = carrera;
+					break;
+				}
+			}
+		}
+		public IActionResult OnPost()
+		{
+			if (!ModelState.IsValid)
+			{
+				return Page();
+			}
+
+			foreach (var carrera in DatosCompartidos.ListCarrera)
+			{
+				if (carrera.Id == oCarrera.Id)
+				{
+					carrera.Nombre = oCarrera.Nombre;
+					carrera.Modalidad = oCarrera.Modalidad;
+					carrera.DuracionAnios = oCarrera.DuracionAnios;
+					carrera.TituloOtorgado = oCarrera.TituloOtorgado;
+					break;
+				}
+			}
+			return RedirectToPage("TablaCarreras");
+		}
+
+	}
+}
---
+++ Pages/Carreras/TablaCarreras.cshtml.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SistemaAcademico.Data;
+using SistemaAcademico.Models;
+
+namespace SistemaAcademico.Pages.Carreras
+{
+	// aguanten las facturas
+	public class TablaCarrerasModel : PageModel
+	{
+		public List<Carrera> ListaMostrarCarrera;
+		public void OnGet()
+		{
+			ListaMostrarCarrera = DatosCompartidos.ListCarrera;
+		}
+
+	}
+}
