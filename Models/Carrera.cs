abbf67f25c1c46d07348fde06a886cd649ac2589
pablobenitezpasserini <pablobenitez252@gmail.com>
2025-05-30 23:19:43 -0300

    fix validacion de campos form carrera

--- Models/Carrera.cs
+++ Models/Carrera.cs
@@ -1,11 +1,19 @@
-﻿namespace SistemaAcademico.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaAcademico.Models
 {
     public class Carrera
     {
         public int Id { get; set; }
-        public string? Nombre { get; set; }
-        public int DuracionAnios { get; set; }
-        public string? TituloOtorgado { get; set; }
-        public string? Modalidad { get; set; }
+		[Required(ErrorMessage = "El nombre no puede estar vacio")]
+		public string? Nombre { get; set; }
+		[Display(Name = "Duración en años")]
+		[Required(ErrorMessage = "La duracion no puede estar vacia")]
+		public int? DuracionAnios { get; set; }
+		[Display(Name = "Titulo otorgado")]
+		[Required(ErrorMessage = "El titulo no puede estar vacio")]
+		public string? TituloOtorgado { get; set; }
+		[Required(ErrorMessage = "La modalidad no puede estar vacia")]
+		public string? Modalidad { get; set; }
     }
 }
